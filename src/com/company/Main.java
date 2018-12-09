f0279ad632ce43ad6390b03ad0820a930e1c9fc5
studychao <503265133@qq.com>
2018-12-09 21:33:49 +0000

    QuickUnion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Quickfind a = new Quickfind(5);
+        QuickUnion a = new QuickUnion(5);
         System.out.print(a.id[0]);
         System.out.print(a.id[4]);
         System.out.print(a.id[3]);
         a.union(0,4);
-
         a.union(3,4);
-        System.out.print(a.id[0]);
+        a.union(4,2);
+        System.out.print(a.root(0));
     }
 }
--- src/com/company/QuickUnion.java
+++ src/com/company/QuickUnion.java
@@ -1,4 +1,33 @@
 package com.company;
 
 public class QuickUnion {
+    public int[] id;
+
+    public QuickUnion(int N)
+    {
+        id = new int[N];
+        for (int i=0;i<N;i++)
+        {
+            id[i] = i;
+        }
+    }
+
+    public int root(int i)
+    {
+        while(i != id[i])
+        {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected (int p,int q)
+    {
+        return root(p) == root(q);
+    }
+
+    public void union (int p,int q)
+    {
+        id[p] = root(q);
+    }
 }
